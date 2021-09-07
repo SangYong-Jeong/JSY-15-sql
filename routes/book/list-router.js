bb4af7083add6100509a773eae6196cd626c9391
SangYong-Jeong <mmaf@naver.com>
2021-09-07 09:30:51 +0900

    수업시작

--- routes/book/list-router.js
+++ routes/book/list-router.js
@@ -4,11 +4,20 @@ const express = require('express')
 const router = express.Router()
 const { error, cutTail, chgStatus } = require('../../modules/util')
 const { pool } = require('../../modules/mysql-init')
+const createPager = require('../../modules/pager-init')
 
 router.get(['/', '/:page'], async (req, res, next) => {
 	try {
-		const sql = 'SELECT * FROM books ORDER BY idx DESC';
-		const [rs] = await pool.execute(sql)
+		let sql = 'SELECT COUNT(idx) FROM books'
+		const [[cnt]] = await pool.execute(sql)
+		const totalRecord = cnt['COUNT(idx)']
+		const page = req.params.page || 1
+		const pager = createPager(page, totalRecord, 2, 3)
+
+		sql = `SELECT * FROM books ORDER BY idx DESC LIMIT ?,?`;
+		const values = [pager.startIdx.toString(), pager.listCnt.toString()]
+		const [rs] = await pool.execute(sql, values)
+
 		const books = rs.map( v => {
 			v.createdAt = moment(v.createdAt).format('YYYY-MM-DD')
 			v.content = cutTail(v.content)
@@ -20,7 +29,8 @@ router.get(['/', '/:page'], async (req, res, next) => {
 		const description = '등록된 도서들의 리스트 입니다.'
 		const js = 'book/list'
 		const css = 'book/list'
-		res.status(200).render('book/list', {title, description, js, css, books})
+		res.status(200).render('book/list', {title, description, js, css, books, pager})
+
 	}
 	catch(err) {
 		next(error(err))

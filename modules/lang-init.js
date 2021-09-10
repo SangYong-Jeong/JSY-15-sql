23a41c6671734a7f67726b88c749826be10b47ab
SangYong-Jeong <mmaf@naver.com>
2021-09-10 14:49:19 +0900

    영문화 작업 v0.3

--- modules/lang-init.js
+++ modules/lang-init.js
@@ -26,6 +26,26 @@ module.exports = _lang => {
 			UPDATE : {
 				TITLE				: '도서 수정',
 				DESC 				: '수정할 도서 내용을 아래에서 변경하세요.',
+			},
+			FIELD : {
+				NO						: '번호',
+				TITLE					: '제목',
+				WRITER				: '저자',
+				CONTENT				: '내용',
+				DATE					: '등록일',
+				COVER					: '표지',
+				STATUS				: '판매',
+				UPFILE				: '첨부파일',
+			},
+			BT : {
+				UPDATE				: '수정',
+				DELETE				: '삭제',
+				CREATE				: '등록',
+				LIST					: '리스트',
+				RESET					: '다시 등록'
+			},
+			MSG : {
+				DELETE				: '정말로 삭제하시겠습니까?'
 			}
 		}
 	case 'EN':
@@ -53,6 +73,26 @@ module.exports = _lang => {
 			UPDATE : {
 				TITLE				: 'Book Update',
 				DESC 				: 'Change the contents of the book to be edited below.',
+			},
+			FIELD : {
+				NO						: 'No',
+				TITLE					: 'Title',
+				WRITER				: 'Writer',
+				CONTENT				: 'Content',
+				DATE					: 'Date',
+				COVER					: 'Cover',
+				STATUS				: 'Status',
+				UPFILE				: 'Attachment file',
+			},
+			BT : {
+				UPDATE				: 'UPDATE',
+				DELETE				: 'DELETE',
+				CREATE				: 'CREATE',
+				LIST					: 'LIST',
+				RESET					: 'RESET'
+			},
+			MSG : {
+				DELETE				: 'Are you sure you want to delete it?'
 			}
 		}
 	}

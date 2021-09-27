fa5f8818f15735bbed609c3c5f941269764901a3
SangYong-Jeong <mmaf@naver.com>
2021-09-27 10:09:25 +0900

    update models

--- routes/auth/login-router.js
+++ routes/auth/login-router.js
@@ -2,9 +2,11 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const { pool } = require('../../modules/mysql-init')
+const { findUser } = require('../../models/auth')
 
-router.get('/', (req, res, next) => { // login 창 보여주기
-
+router.get('/', async (req, res, next) => { // login 창 보여주기
+	await findUser('id', 1)
+	res.send('error')
 })
 
 router.post('/', (req, res, next) => { // 실제 login 로직

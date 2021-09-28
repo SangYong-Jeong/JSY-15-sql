9fbcdbd18815b48bf916a02ee5bd1e9b3257e7a4
SangYong-Jeong <mmaf@naver.com>
2021-09-28 15:42:06 +0900

    localStrategy 완료

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
+const passport = require('passport')
+const passportModule = require('./passport')
+
 const langMW = require('./middlewares/lang-mw')
 const method = require('./middlewares/method-mw')
 const logger = require('./middlewares/morgan-mw')
@@ -25,6 +28,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(method())	// method-override
 app.use(session(app))
+
+/*************** passport ***************/
+passportModule(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+/*************** locals ***************/
 app.use(locals)
 
 
@@ -53,6 +64,7 @@ app.use('/api/auth', apiAuthRouter)
 /**************** error init **************/
 const _404Router = require('./routes/error/404-router')
 const _500Router = require('./routes/error/500-router')
+const { Passport } = require('passport')
 
 app.use(_404Router)
 app.use(_500Router)

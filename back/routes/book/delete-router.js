5611a604071b691c97a9b1fed8716f4c54d0e03e
SangYong-Jeong <mmaf@naver.com>
2021-12-14 12:58:12 +0900

    delete router 수정

--- back/routes/book/delete-router.js
+++ back/routes/book/delete-router.js
@@ -1,30 +1,25 @@
-const path = require('path')
-const fs = require('fs-extra')
-const express = require('express')
-const createError = require('http-errors')
-const router = express.Router()
-const { moveFile } = require('../../modules/util')
-const { isUser, isGuest, isMyBook } = require('../../middlewares/auth-mw')
-const {updateBookStatus} = require('../../models/book')
-const {updateFileStatus, findBookFiles} = require('../../models/file')
+const path = require('path');
+const fs = require('fs-extra');
+const express = require('express');
+const createError = require('http-errors');
+const router = express.Router();
+const { moveFile } = require('../../modules/util');
+const { updateBookStatus } = require('../../models/book');
+const { updateFileStatus, findBookFiles } = require('../../models/file');
+const { isUser, isGuest, isMyBook } = require('../../middlewares/auth-mw');
 
 router.delete('/', isUser, isMyBook('body'), async (req, res, next) => {
-	let sql
-	try {
-		// sql = "DELETE FROM books WHERE idx=?"
-		sql = "UPDATE books SET status='0' WHERE idx = " + req.body.idx
-		await updateBookStatus(req.body.idx)
-		await updateFileStatus(req.body.idx)
-		const {files} = await findBookFiles(req.body.idx)
+  try {
+    await updateBookStatus(req.body.idx);
+    await updateFileStatus(req.body.idx);
+    const { files } = await findBookFiles(req.body.idx);
+    for (let { savename } of files) {
+      await moveFile(savename);
+    }
+    res.redirect(`/${req.lang}/book`);
+  } catch (err) {
+    next(createError(err));
+  }
+});
 
-		for(let { savename } of files) {
-			await moveFile(savename)
-		}
-		res.redirect(`/${req.lang}/book`)
-	}
-	catch(err) {
-		next(createError(err))
-	}
-})
-
-module.exports = router
+module.exports = router;
--- back/routes/book/index.js
+++ back/routes/book/index.js
@@ -1,24 +1,24 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
+const path = require('path');
+const express = require('express');
+const router = express.Router();
 
-const formRouter = require('./form-router')
-const listRouter = require('./list-router')
-const viewRouter = require('./view-router')
-const downloadRouter = require('./download-router')
-const saveRouter = require('./save-router')
-const deleteRouter = require('./delete-router')
+const formRouter = require('./form-router');
+const listRouter = require('./list-router');
+const viewRouter = require('./view-router');
+const downloadRouter = require('./download-router');
+const saveRouter = require('./save-router');
+const deleteRouter = require('./delete-router');
 
 // static 주소는 use로 받아서 전달해야한다. 가변은 get으로 받아도 상관없다.
 
-router.post('/', saveRouter) 												// FORM/POST: 저장, 수정
-router.delete('/', deleteRouter) 										// FORM/DELETE: 삭제
-router.use('/form', formRouter) 										// HTML: 글작성(수정)페이지
-router.use('/view', viewRouter) 										// HTML/GET: 상세페이지
-router.use('/download', downloadRouter) 						// HTML/GET: 상세페이지
-router.use('/', listRouter) 												// HTML/GET: 리스트페이지
+router.post('/', saveRouter); // FORM/POST: 저장, 수정
+router.delete('/', deleteRouter); // FORM/DELETE: 삭제
+router.use('/form', formRouter); // HTML: 글작성(수정)페이지
+router.use('/view', viewRouter); // HTML/GET: 상세페이지
+router.use('/download', downloadRouter); // HTML/GET: 상세페이지
+router.use('/', listRouter); // HTML/GET: 리스트페이지
 
-module.exports = router
+module.exports = router;
 
 /*  Restful 진영에서는 주소를 method로 구분하는 것을 권장하고 있다.
 
